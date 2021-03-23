6c89b52d1b6aa7087ca51aa14fbf65f25e535324
mackenzie66 <77417131+mackenzie66@users.noreply.github.com>
2021-03-22 22:37:12 -0600

    Add files via upload

---
+++ HOMEWORK 10/js/*sketch.js
@@ -0,0 +1,64 @@
+function setup()
+            {
+                createCanvas(500, 600);
+            }
+
+            function draw()
+            {
+
+                background(120,45,78); 
+                fill('#fae');
+                circle(250,100,170);
+
+                fill(60, 20, 600);
+                circle(225,80, 40);
+                circle(275,80, 40);
+
+                fill(10);
+                circle(225,80, 20);
+                circle(275, 80, 20)
+
+                fill('red')
+                triangle(270, 105, 250, 140, 230, 105);
+
+                fill('rgba(0,255,0, 0.25)');
+                rect(190, 189, 130,200);
+                rect(320, 200, 120,50);
+                rect(70, 200, 120,50);
+                rect(190, 389, 50,180);
+                rect(270, 389, 50,180);
+
+                line(200, 130, 230, 160);
+                line(230, 160, 270, 160);
+                line(270, 160, 295, 130);
+
+                fill(10);
+                point(220, 120);
+                point(278, 120);
+                point(270, 130);
+                point(230, 130);
+                point(212, 130);
+                point(285, 130);
+
+                fill(10);
+                line(220, 20, 160, 190);
+                line(210, 25, 150, 190);
+                line(200, 30, 140, 190);
+                line(190, 40, 130, 190);
+
+                line(280, 20, 340, 190);
+                line(290, 25, 350, 190);
+                line(300, 30, 360, 190);
+                line(310, 40, 370, 190);
+
+                fill(10);
+                textSize(32);
+                text('Self Portrait', 10, 30);
+
+                textSize(20);
+                text('MacKenzie Dean', 340, 580);
+
+                
+
+                
+            }
