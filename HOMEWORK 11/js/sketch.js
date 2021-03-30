dc270510e73ae78c2efb0c93464f0438779e7655
mackenzie66 <77417131+mackenzie66@users.noreply.github.com>
2021-03-29 18:40:53 -0600

    Add files via upload

---
+++ HOMEWORK 11/js/sketch.js
@@ -0,0 +1,116 @@
+// x and y for my character
+var characterX = 100;
+var characterY = 100;
+// define the key codes for each letter
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
+
+// x and y for a shape
+var shapeX = 30;
+var shapeY = 50;
+var shapeXSpeed;
+var shapeYSpeed;
+
+// create a shape when the mouse is clicked
+var mouseShapeX;
+var mouseShapeY;
+function setup()
+{
+    createCanvas(500, 600);
+    // get a random speed when the it first starts
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+}
+
+function draw()
+{
+    background(160,80,180);
+    stroke(0);
+    fill(0);
+    // top border
+    rect(0,0,width,10);
+    // left border
+    rect(0,0,10,height);
+    // bottom border
+    rect(0, height-10,width, 10);
+    // right upper border
+    rect(width-10,0,10,height-50);
+
+    // exit message
+    textSize(16);
+    text("EXIT", width-50,height-50)
+
+    //character
+    fill(26,300,10);
+    square(characterX,characterY,30);
+
+    // handle the keys
+    if(keyIsDown(w))
+    {
+        characterY -= 10;   
+    }
+    if(keyIsDown(s))
+    {
+        characterY += 10;   
+    }
+    if(keyIsDown(a))
+    {
+        characterX -= 10;   
+    }
+    if(keyIsDown(d))
+    {
+        characterX += 10;   
+    }
+
+    // potential enemy
+    fill(90,30,300);
+    // draw the shape
+    circle(shapeX, shapeY, 15);
+
+     // get a random speed when the it first starts
+     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+
+    // move the shape
+    shapeX += shapeXSpeed;
+    shapeY += shapeYSpeed;
+    // check to see if the shape has gone out of bounds
+    if(shapeX > width)
+    {
+        shapeX = 0;
+    }
+    if(shapeX < 0)
+    {
+        shapeX = width;
+    }
+    if(shapeY > height)
+    {
+        shapeY = 0;
+    }
+    if(shapeY < 0)
+    {
+        shapeY = height;
+    }
+
+    // check to see if the character has left the exit
+    if(characterX > width && characterY > width-50)
+    {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("You Win!", width/2-50, height/2-50);
+    }
+
+    // create the shape based on the mouse click
+    fill(12,12,40);
+    circle(mouseShapeX, mouseShapeY, 25);
+}
+
+function mouseClicked()
+{
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
