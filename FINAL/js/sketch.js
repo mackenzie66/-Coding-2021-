5a823841dfe13c2607aa5e7979a566149c29a254
mackenzie66 <77417131+mackenzie66@users.noreply.github.com>
2021-04-30 22:57:16 -0600

    Add files via upload

---
+++ FINAL/js/sketch.js
@@ -0,0 +1,58 @@
+function setup()
+    {
+        createCanvas(500,600);
+    }
+    
+    function draw ()
+    {
+        background(120,45,78); 
+        fill (255, 204, 0);
+        circle(410,90,90);
+
+    
+        fill(10);
+        line (360,120,310,160);
+        line (425,140,430,210);
+        line (460,90,560,90);
+        line (310,20,370,60);
+        line (435,45,460,10);
+
+        fill('rgb(0,255,0)');
+        rect (0,450,500,150);
+
+        fill(10);
+        line (100,450,100,370);
+        line (250,450,250,370);
+        line (400,450,400,370);
+
+        
+
+        fill(color(0,0,255));
+        circle(100,300,50);
+        circle(145,340,50);
+        circle(125,390,50);
+        circle(75,390,50);
+        circle(50,340,50);
+
+        circle(250,300,50);
+        circle(290,340,50);
+        circle(275,390,50);
+        circle(225,390,50);
+        circle(210,340,50);
+
+        circle(400,300,50);
+        circle(440,340,50);
+        circle(425,390,50);
+        circle(375,390,50);
+        circle(360,340,50);
+
+        fill('#fae');
+        circle(100,350,70);
+        circle(250,350,70);
+        circle(400,350,70);
+
+        fill('red');
+        textSize(60);
+        text('HAPPY',20,100);
+        text('SUMMER!',20,170);
+    }      
